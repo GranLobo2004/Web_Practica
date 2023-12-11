c29cdd347d344f1d9d531ea6700e772f7a59a9aa
Alexander Pearson <alexanderpearson2828@gmail.com>
2023-12-11 13:14:47 +0100

    errores de campos en formulario

--- src/formularioRouter.js
+++ src/formularioRouter.js
@@ -60,7 +60,18 @@ router.post('/paginaDetalle/edit/:id', (req, res) => {
     res.render('Formulario', { producto })
 });
 
-router.post('/producto/edited/:id',(req,res) =>{
+import { body, validationResult } from 'express-validator';
+
+router.post('/producto/edited/:id',[
+    body('nombre').notEmpty().withMessage('El nombre es obligatorio'),
+    body('vendedor').notEmpty().withMessage('El nombre del vendedor es obligatorio'),
+    body('precio').notEmpty().withMessage('El precio es obligatorio'),
+    body('categoria').notEmpty().withMessage('La categoría es obligatoria'),
+    // Agrega más reglas de validación según tus necesidades
+  ], (req,res) =>{
+    const errors = validationResult(req);
+   
+
     let { nombre, precio, vendedor, categoria, descripcion, servicios, estado, imagen1, imagen2, imagen3 } = req.body;
     let id = req.params.id;
     let imagenes;
@@ -75,7 +86,13 @@ router.post('/producto/edited/:id',(req,res) =>{
     var num = +id;
     let producto = {id:num, nombre:nombre, precio:precio, vendedor:vendedor, categoria:categoria, descripcion:descripcion, servicios:servicios, estado:estado, imagenprincipal:imagen1, imagenes:imagenes};
     productos.editProducto(producto);
-    res.render('paginaDetalle',{producto});
+    if (!errors.isEmpty()) {
+        // Hay errores de validación, adjunta los mensajes de error al objeto res.locals
+        res.locals.errors = errors.array();
+        res.render('Formulario', { producto: req.body, errors: res.locals.errors });
+      }
+      else
+      res.render('paginaDetalle', { producto });
 })
 
 
