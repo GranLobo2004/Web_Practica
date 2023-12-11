bc779c4f1ceed03c110730e365fa0cc673443849
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2023-12-11 10:39:39 +0100

    fuera checkpoints

--- src/formularioRouter.js
+++ src/formularioRouter.js
@@ -3,9 +3,7 @@ import * as productos from './Service.js';
 
 const router = express.Router();
 router.get('/Formulario', (req, res) => {
-
-    console.log(req.body);
-
+    
     res.render('Formulario');
 });
 
